59d2415c638fa7c9e1db9863aad37ddd031e60eb
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-06-05 23:18:22 +0530

    controller changes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FeedController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class , 'index']);
 Route::get('/profile', [ProfileController::class , 'index']);
-Route::get('/feed', [ProfileController::class , 'index']);
+Route::get('/feed', [FeedController::class , 'index']);
 
 // Route::get('/feed', function () {
 //     return view('feed');
